e694cc8fdf27e5a8f6e6882ffb955d09fe29f4ec
Dean Sundquist <dsundquist@cloudflare.com>
2021-12-07 18:08:19 -0600

    First commit moving over to Github / CF Pages

---
+++ index.php
@@ -0,0 +1,111 @@
+<html>
+
+
+<head>
+        <title>- Sundquist.net</title>    
+    	<link rel="stylesheet" type="text/css" href="./style2.css">
+
+		<link href="https://fonts.googleapis.com/css?family=Noto+Sans" rel="stylesheet">
+		<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+		<link href="https://fonts.googleapis.com/css?family=Permanent+Marker" rel="stylesheet">
+
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<!--		<script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.7.2/p5.js"></script>--> 
+</head>
+    
+    
+    <body>
+        
+        
+        
+        <!--- Header -->
+        <div class="header">
+            <a href="https://www.sundquist.net" id="name">
+                <i class="material-icons" style="color: white; font-size: 35px;">home</i>
+            
+            </a>
+            <a href="#login" id="links" onclick="show_the_overlay();">
+<!--                <i class="material-icons" style="color: black">account_circle</i> -->
+                login </a> 
+            <a href="#left" id="links">
+<!--                <i class="material-icons" style="color: black">face</i> -->
+                about</a>
+            <a href="mailto:dean@sundquist.net" id="links">
+<!--                <i class="material-icons" style="color: black">email</i> -->
+                contact</a>
+        </div>
+
+
+        
+        <!-- First -->
+        <div class="first" style="">
+            <div id="about"></div> <!-- for jumping to -->
+            <div class="title"><bold>Dean Sundquist</bold></div>
+        </div>        
+ 
+        
+        
+        <!-- Second -->
+        <div class="second">            
+            
+             <div id="left"> 
+            <!---Left --->
+                 
+                 
+                 <center><img src="./headshot.png" width=75%></center>
+           
+                 <b><h2>Education</h2></b>  
+                
+                 <li><a href="https://www.wisc.edu" style="color:black; text-decoration: none; font-weight: bold;">University of Wisconsin- Madison</a>, August 2014 Bachelor of Science, <br>Major: Computer Science</li>
+                 <li><a href="https://www.madisoncollege.edu" style="color:black; text-decoration: none; font-weight: bold;">Madison College</a>, August 2007 Attended Transfer Program, <br> Focus: Network Specialist and Security</li>
+                 
+                <br>
+                <br>
+                 <center><img src="./signature.png"></center>
+
+
+            <!---End Left --->         
+            </div>
+            <div id="right">
+            <!---Right --->
+                
+                <p style="font-size: 20px" weight="bold">I am a <b>network engineer</b>, programmer, and overall computer enthusiast.  Here you can find basic information about me along with some contact information, and current projects that I am working on.</p>
+                 
+				 <a href="https://www.deansundquist.com" style="color:black; text-decoration: none; font-weight: bold;">Some projects (needs updating)</a></li>                
+ 
+                 
+                <h2>Computer Knowledge and Skills </h2>
+                <li>Computer Networking and Security</li>
+                <li>Database Management Systems</li>
+                <li>C, C++, Java, C#, Python, JavaScript, HTML, CSS</li>
+                <li>Calculus, Linear Algebra</li>
+                
+
+
+            <!-- Content3 -->
+
+            <h2>Highlights</h2>
+	       
+					<li> Currently I work as a <b>Technical Support Engineer</b> at <a href="https://www.cloudflare.com" style="color:black; text-decoration: none; font-weight: bold;">Cloudflare</a></li>
+					<li>Previously a Network Engineer I/II at <a href="https://www.corebts.com" style="color:black; text-decoration: none; font-weight: bold;">Core BTS,</a></li> where I responded to high severity incidents, primarily focusing on Cisco Firewalls, Routers, and Switches.       
+					<li>Effectively collaborated and coordinated multi-user programming projects in class and workplace.</li>
+					<li>Large breadth of communication classes. Letters and Science, <a href="https://www.wisc.edu" style="color:black; text-decoration: none; font-weight: bold;">UW -Madison</a></li>
+                    <li>Extensive customer service experiences</li>
+               
+            <!---End Right -->
+            </div>
+        </div>
+
+        <div class="footer">
+            <!-- img src="./face.png" alt="" -->
+            <div id="name">Website design by Dean Sundquist
+            </div>
+            <a href="mailto:dean@sundquist.net" style="color: grey"><i class="fa fa-envelope"></i></a>				
+            <a href="https://www.reddit.com/user/ubrpwnzr" style="color: grey"><i class="fa fa-reddit"></i></a>
+            <a href="https://github.com/dsundquist" style="color: grey"><i class="fa fa-github"></i></a>
+            <a href="https://linkedin.com/in/deansundquist/" style="color: grey"><i class="fa fa-linkedin"></i></a>
+        </div>
+        
+    </body>
+</html>
